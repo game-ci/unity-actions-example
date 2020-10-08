d8a020a4d6b61950c5352d61fea7d2ab6e4f0da9
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2020-10-08 10:26:03 -0400

    Add a very basic game example (A winner is you)

---
+++ Assets/OnCollisionEnterEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class OnCollisionEnterEvent : MonoBehaviour
+{
+    [SerializeField] private UnityEvent @event;
+    [SerializeField] private string otherGameObjectName = "Sphere";
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == otherGameObjectName)
+        {
+            @event.Invoke();
+        }
+    }
+}
---
+++ Assets/YouWin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameObjectToEnable;
+    
+    public void Run()
+    {
+        gameObjectToEnable.SetActive(true);
+    }
+}
